f12e9ba2e15c97ed0d835a84cb27426accd23f35
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-17 15:17:03 +0000

    added /name_cat pathway

--- app.rb
+++ app.rb
@@ -12,13 +12,13 @@ get "/reallysecret" do
   "This is really secret!"
 end
 
-get "/cat" do
-  %q{
-    <head>
-    <link rel="stylesheet" type="text/css" href="/style.css">
-    </head>
-    <img src="http://bit.ly/1eze8aE" class="imageborder" />
+get "/random_cat" do
+  @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
 
-  }
-
+get "/named_cat" do
+  p params
+  @cat_name = params[:name]
+  erb(:named_cat)
 end
